ac5598e5d245661c8ec0243c0d4f01fa789d757e
SaharOladi <sahar.olady@gmail.com>
2020-09-02 19:52:28 +0430

    add viewHolder and Adapter.

--- app/src/main/java/com/example/hw10/controller/activity/TaskActivity.java
+++ app/src/main/java/com/example/hw10/controller/activity/TaskActivity.java
@@ -2,18 +2,26 @@ package com.example.hw10.controller.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.hw10.R;
-import com.example.hw10.controller.fragment.MainActivityFragment;
 import com.example.hw10.controller.fragment.TaskFragment;
 
+import static com.example.hw10.controller.fragment.MainActivityFragment.EXTRA_NUMBER_TASK;
+
 public class TaskActivity extends AppCompatActivity {
+
+
+    public static final String EXTRA_COUNT_TASK = "EXTRA_COUNT_TASK";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
+
+
+        sendIntent();
 
         TaskFragment taskFragment = new TaskFragment();
 
@@ -21,5 +29,11 @@ public class TaskActivity extends AppCompatActivity {
                 .beginTransaction()
                 .add(R.id.task_activity, taskFragment)
                 .commit();
+    }
+
+    private void sendIntent() {
+        int count = Integer.parseInt(getIntent().getExtras().getString(EXTRA_NUMBER_TASK));
+        Intent intent = new Intent(TaskActivity.this, TaskFragment.class);
+        intent.putExtra(EXTRA_COUNT_TASK, count);
     }
 }
--- app/src/main/java/com/example/hw10/controller/fragment/TaskFragment.java
+++ app/src/main/java/com/example/hw10/controller/fragment/TaskFragment.java
@@ -2,14 +2,21 @@ package com.example.hw10.controller.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.hw10.R;
+
+import static com.example.hw10.controller.activity.TaskActivity.EXTRA_COUNT_TASK;
+import static com.example.hw10.controller.fragment.MainActivityFragment.EXTRA_NUMBER_TASK;
 
 
 public class TaskFragment extends Fragment {
@@ -34,12 +41,53 @@ public class TaskFragment extends Fragment {
         View view =  inflater.inflate(R.layout.fragment_task, container, false);
 
         findViews(view);
+        initViews();
 
-        
+
         return view;
+    }
+
+    private void initViews() {
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
     }
 
     private void findViews(View view){
         mRecyclerView = view.findViewById(R.id.task_fragment_recyclerview);
+    }
+
+
+    private class TaskHolder extends RecyclerView.ViewHolder{
+
+        private EditText mTaskName;
+        private TextView mTaskState;
+
+        public TaskHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mTaskName = itemView.findViewById(R.id.row_task_name);
+            mTaskState = itemView.findViewById(R.id.row_task_state);
+
+        }
+    }
+
+    private class TaskAdapter extends RecyclerView.Adapter<TaskHolder>{
+
+        private int count = Integer.parseInt(getActivity().getIntent().getExtras().getString(EXTRA_COUNT_TASK));
+
+        @NonNull
+        @Override
+        public TaskHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull TaskHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return count;
+        }
     }
 }
